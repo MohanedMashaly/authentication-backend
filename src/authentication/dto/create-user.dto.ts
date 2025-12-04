224f483800a2251e8c5a063259b5589e481a7025
MohanedMashaly <mohaned.mashaly12@gmail.com>
2025-12-04 17:11:46 +0200

    [docs][swagger]: Add swagger docs for api

--- src/authentication/dto/create-user.dto.ts
+++ src/authentication/dto/create-user.dto.ts
@@ -1,16 +1,29 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsOptional, IsString, IsStrongPassword, Matches, Min } from "class-validator";
 
 export class CreateUserDto{
     @IsEmail()
+    @ApiProperty({
+        description: 'Email of the user',
+        required: true,
+    })
     email:string;
 
     @IsString()
     @Min(3)
+    @ApiProperty({
+        description: 'Name of the user',
+        required: true,
+    })
     name:string;
 
     @Matches(
     /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
     { message: 'Password too weak. Must contain at least 8 characters, one letter, one number, and one special character.'})
+    @ApiProperty({
+        description: 'Password, password must meet the following criteria(least 8 characters, one letter, one number, and one special character)',
+        required: true,
+    })
     password:string;
 
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { INestApplication, Logger } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+
+export function setupSwagger(app: INestApplication, version:any): any {
+  const logger: Logger = new Logger('Swagger');
+  const swaggerEndpoint = '/swagger';
+
+  const options = new DocumentBuilder()
+    .setTitle('API Service')
+    .setDescription('API Service documentation')
+    .setVersion(version)
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup(swaggerEndpoint, app, document);
+  logger.log(`Added swagger on endpoint ${swaggerEndpoint}`);
+}
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  setupSwagger(app, '1.0.0');
+  await app.listen(3009);
 }
 bootstrap();
